2256223cef3ee773bf757d886360f487a35388e1
Smrutimedha <31215014+Smrutimedha@users.noreply.github.com>
2017-08-21 21:59:50 +0530

    Create Lab1_question5.cpp

---
+++ Lab1_question5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double l,b,area;
+	cout << "Enter length and breadth of rectangle" << "\n";
+	cin >> l >> b;
+	area=l*b;
+	cout << "The area is " << area;
+	return 0;
+  }
